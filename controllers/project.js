251fea17e24b2cc1f0146e9dd32ac5ea68ead495
toni <ferreirocouto@gmail.com>
2021-02-16 19:18:58 +0100

    añadida visualizacion imagen

--- controllers/project.js
+++ controllers/project.js
@@ -3,6 +3,8 @@
 var Project = require('../models/project');
 
 var fs = require('fs');
+var path = require('path');
+const { exists } = require('../models/project');
 
 var controller = {
     home: function(req, res){
@@ -153,6 +155,21 @@ var controller = {
                 message: fileName
             });
         }
+    },
+
+    getImageFile: function(req, res){
+        var file = req.params.image;
+        var path_file = './uploads/'+file;
+        
+        fs.stat(path_file, (exists) => {
+            if (exists){
+                return res.sendFile(path.resolve(path_file));
+            } else {
+                return res.status(200).send({
+                    message: "No existe la imagen.."
+                });
+            }
+        });
     }
 };
 
